40e07aeb17c214d0c6ba04d96b2a5cd7c52239fb
Joel Leblanc <leblanc.js@icloud.com>
2022-11-28 13:14:25 -0500

    Added a LoginButton, LogoutButton and Profile component to test integration with the Auth0 SDK.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import './App.css'
+import LoginButton from './LoginButton'
+import LogoutButton from './LogoutButton'
+import Profile from './Profile'
 
 function App() {
-  return <div>Hi, mom!</div>
+  return (
+    <div>
+      <span>Hi, mom!</span>
+      <LoginButton />
+      <LogoutButton />
+      <Profile />
+    </div>
+  )
 }
 
 export default App
